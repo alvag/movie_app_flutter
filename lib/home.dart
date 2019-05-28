4d4cbcd8ff26ab7c84f3ce770eacd46ce79b2e26
Max Alva <alva85@gmail.com>
2019-05-28 01:18:37 -0500

    BottomNavigationBar y Drawer

---
+++ lib/home.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+    @override
+    _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+    @override
+    Widget build(BuildContext context) {
+        return new Scaffold(
+            appBar: new AppBar(
+                title: new Text('Flutter Movie App'),
+                actions: <Widget>[
+                    new IconButton(
+                        icon: new Icon(Icons.search, color: Colors.white),
+                        onPressed: () {}
+                    )
+                ],
+            ),
+            drawer: new Drawer(
+                child: new ListView(
+                    children: <Widget>[
+                        new DrawerHeader(child: new Material()),
+                        new ListTile(
+                            title: new Text('Películas'),
+                            trailing: new Icon(Icons.local_movies),
+                        ),
+                        new Divider(
+                            height: 5,
+                        ),
+                        new ListTile(
+                            title: new Text('Series'),
+                            trailing: new Icon(Icons.live_tv),
+                        ),
+                        new Divider(
+                            height: 5,
+                        ),
+                        new ListTile(
+                            title: new Text('Cerrar'),
+                            trailing: new Icon(Icons.close),
+                            onTap: () => Navigator.of(context).pop(),
+                        ),
+                    ],
+                ),
+            ),
+            bottomNavigationBar: new BottomNavigationBar(
+                items: _getFooterItems()
+            ),
+        );
+    }
+
+    List<BottomNavigationBarItem> _getFooterItems() {
+        return [
+            new BottomNavigationBarItem(
+                icon: new Icon(Icons.thumb_up),
+                title: new Text('Populares')
+            ),
+            new BottomNavigationBarItem(
+                icon: new Icon(Icons.update),
+                title: new Text('Proximamente')
+            ),
+            new BottomNavigationBarItem(
+                icon: new Icon(Icons.star),
+                title: new Text('Mejor valoradas')
+            )
+        ];
+    }
+}
