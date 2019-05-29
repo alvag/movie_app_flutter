93d3f9dfa05f4ff010f016e5d7872ea3dba28df4
Max Alva <alva85@gmail.com>
2019-05-28 23:03:47 -0500

    Http request

--- lib/common/HttpHandler.dart
+++ lib/common/HttpHandler.dart
@@ -1,12 +1,27 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:movie_app_flutter/common/env.dart';
 
 class HttpHandler {
+
+    final String _baseUrl = 'api.themoviedb.org';
+    final String _language = 'es-ES';
 
     Future<dynamic> getJson(Uri uri) async {
         http.Response response = await http.get(uri);
         return json.decode(response.body).toString();
+    }
+
+    Future<String> fetchMovies() {
+        var uri = new Uri.https(_baseUrl, '3/movie/popular', {
+            'api_key': API_KEY,
+            'page': '1',
+            'language': _language
+        });
+
+        return getJson(uri)
+            .then(((response) => response.toString()));
     }
 
 }
